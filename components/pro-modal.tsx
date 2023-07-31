ba871ddc5bca37e6858af45e9161bd4c55320aa3
justmogen <justmogen@gmail.com>
2023-07-31 09:05:47 +0300

    updated

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -32,6 +32,7 @@ export const ProModal = () => {
       window.location.href = response.data.url;
     } catch (error) {
    //   toast.error("Something went wrong");
+      console.log(error,"STRIPE_CLIENT_ERROR")
     } finally {
       setLoading(false);
     }
@@ -66,7 +67,7 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button disabled={loading} onClick={onSubscribe} size="lg" variant="premium" className="w-full">
+          <Button onClick={onSubscribe} size="lg" variant="premium" className="w-full">
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
